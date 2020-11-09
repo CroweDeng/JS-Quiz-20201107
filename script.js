d97e919ca9d57babcf54c50795856ced5a080a1c
Crowe Deng <crowe.deng@neutec.comtw>
2020-11-09 20:33:22 +0800

    printStars2_crowedeng

--- script.js
+++ script.js
@@ -13,7 +13,26 @@ function printStars(count) {
 
 // 請參考 printStars，
 // 並完成 printStars2，使其執行時結果如圖： https://imgur.com/56BptSu
-function printStars2(count) {
-  // todo...
-  
-}
+
+function  printStars2(count) {
+      // todo...
+      let str = '';
+      let space = ' ';
+      let i, j, s, h;
+
+      for (i = 1; i <= count; i++) {
+        // console.log('count', count);
+        
+        for (h = 5; h > i; h--) {
+          // console.log('h', h);
+          str = str + ' ';
+        }
+
+        for (j = 1; j <= i; j++) {          
+          // console.log('j',j);
+          str = str + '*';
+        }
+        str += '\n';
+      }
+      console.log(str);
+    }
